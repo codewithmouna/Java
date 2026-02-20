974c6c418e8d5f2ad463f4a20983c69219417b5b
CodeWithMouna~ <mc.mouna98@gmail.com>
2026-02-20 11:58:48 +0530

    Added:Conditional statements

--- JavaLearn_7/src/SwitchExpressionDemo.java
+++ JavaLearn_7/src/SwitchExpressionDemo.java
@@ -2,13 +2,7 @@ public class SwitchExpressionDemo {
 
     public static void main(String[] args) {
         String fruitName = "Banana";
-        switch (fruitName) {
-            case "Banana", "Apple" -> System.out.println("$ 1.0 charged");
-            case "Grapes" -> System.out.println("$ 2.0 charged");
-            case "Pineapple" -> System.out.println("$ 2.5 charged");
-            case "Mango" -> System.out.println("$ 3.0 charged");
-            default -> System.out.println("Pick a valid fruit");
-        }
+        System.out.println("$ 1.0 charged");
 
         String output = switch (fruitName) {
             case "Banana", "Apple" -> "$ 1.0 charged";
